9cd425cf8c40eaca3cdaa9a31a2d2c45db6a9727
Max Cotton <maxcotton22@gmail.com>
2025-06-01 21:49:43 +0100

    feat: Setup basic web application

---
+++ src/main/java/com/cmp/synopticproject/SynopticprojectApplication.java
@@ -0,0 +1,13 @@
+package com.cmp.synopticproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynopticprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynopticprojectApplication.class, args);
+	}
+
+}
