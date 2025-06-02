af86f636faf5dce45a16267ac141ad3097625b2e
TobyForsyth <tobyaforsyth@gmail.com>
2025-06-02 16:22:17 +0100

    Update SQL_DDL.sql

--- SQL_DDL.sql
+++ SQL_DDL.sql
@@ -1,16 +1,16 @@
-SET search_path TO 'synop25', public;
-
+-- Table Definition
 CREATE TABLE toiletBlock(
 	blockID INTEGER PRIMARY KEY,
 	blockAddress VARCHAR(100) NOT NULL,
-	toiletCount INTEGER,
-	blockStatus VARCHAR(15) DEFAULT 'Closed');
+	blockStatus VARCHAR(15) DEFAULT 'Closed',
+	toiletCount INTEGER DEFAULT 0,
+	CHECK (toiletCount >= 0));
 
 CREATE TABLE toilet(
 	toiletID INTEGER PRIMARY KEY,
 	blockID INTEGER NOT NULL,
 	toiletStatus VARCHAR(15) DEFAULT 'Disabled',
-	FOREIGN KEY (blockID) REFERENCES toiletBlock);
+	FOREIGN KEY (blockID) REFERENCES toiletBlock ON DELETE CASCADE);
 
 CREATE TABLE resident(
 	resID INTEGER PRIMARY KEY,
@@ -22,6 +22,38 @@ CREATE TABLE resident(
 CREATE TABLE farmer(
 	farmerID INTEGER PRIMARY KEY,
 	resID INTEGER,
-	username VARCHAR(32) NOT NULL,
-	password VARCHAR(32) NOT NULL,
 	FOREIGN KEY (resID) REFERENCES resident);
+
+-- Adding a toilet
+CREATE OR REPLACE FUNCTION add_toilet_func()
+RETURNS TRIGGER AS $$
+BEGIN
+	UPDATE toiletBlock
+	SET toiletCount = toiletCount + 1
+	WHERE toiletBlock.blockID = NEW.blockID;
+	RETURN NEW;
+END;
+$$
+LANGUAGE PLPGSQL;
+
+CREATE OR REPLACE TRIGGER add_toilet_trig
+AFTER INSERT ON toilet
+FOR EACH ROW
+EXECUTE FUNCTION add_toilet_func();
+
+-- Removing a toilet
+CREATE OR REPLACE FUNCTION remove_toilet_func()
+RETURNS TRIGGER AS $$
+BEGIN
+	UPDATE toiletblock
+	SET toiletCount = toiletCount - 1
+	WHERE toiletblock.blockID = OLD.blockID;
+	RETURN OLD;
+END;
+$$
+LANGUAGE PLPGSQL;
+
+CREATE OR REPLACE TRIGGER remove_toilet_trig
+AFTER DELETE ON toilet
+FOR EACH ROW
+EXECUTE FUNCTION remove_toilet_func();
