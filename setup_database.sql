c0be1d6892074e7c2cf5c864956774a0cb8d3fc9
Max Cotton <maxcotton22@gmail.com>
2025-06-02 02:01:26 +0100

    chore: Add basic SQL statements to setup database

---
+++ setup_database.sql
@@ -0,0 +1,11 @@
+CREATE TABLE toilet (
+	id INTEGER PRIMARY KEY,
+    location VARCHAR(200) NOT NULL
+);
+
+INSERT INTO toilet VALUES (1, 'location:1');
+INSERT INTO toilet VALUES (2, 'location:2');
+INSERT INTO toilet VALUES (3, 'location:3');
+INSERT INTO toilet VALUES (4, 'location:4');
+INSERT INTO toilet VALUES (5, 'location:5');
+
