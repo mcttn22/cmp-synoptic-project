f0daddca4c308c0f5f7c0d5873e42e28a659c4cf
Max Cotton <maxcotton22@gmail.com>
2025-06-04 23:53:42 +0100

    chore: Update routing for new HTML files

--- src/main/java/com/cmp/synopticproject/WebPageController.java
+++ src/main/java/com/cmp/synopticproject/WebPageController.java
@@ -18,19 +18,35 @@ public class WebPageController {
 	}
 
 	/**
-	 * Render the login page.
-	 */
-	@GetMapping("/login")
-	public String getLogin() {
-		return "login";
-	}
-
-	/**
 	 * Render the ourGoal page.
 	 */
 	@GetMapping("/ourGoal")
 	public String getOurgoal() {
 		return "ourGoal";
+	}
+
+	/**
+	 * Render the report page.
+	 */
+	@GetMapping("/report")
+	public String getReport() {
+		return "report";
+	}
+
+	/**
+	 * Render the welcomeF page.
+	 */
+	@GetMapping("/welcomeF")
+	public String getWelcomeF() {
+		return "welcomeF";
+	}
+
+	/**
+	 * Render the welcomeR page.
+	 */
+	@GetMapping("/welcomeR")
+	public String getWelcomeR() {
+		return "welcomeR";
 	}
 }
 
