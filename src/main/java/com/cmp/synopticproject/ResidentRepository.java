e034c5dd019a4136cad84ab4afebcc331eb92899
Max Cotton <maxcotton22@gmail.com>
2025-06-05 19:13:09 +0100

    chore: Add existsByUsername query to resident repository

--- src/main/java/com/cmp/synopticproject/ResidentRepository.java
+++ src/main/java/com/cmp/synopticproject/ResidentRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 /**
  * Interface to define queries for resident entity.
  */
-public interface ResidentRepository extends JpaRepository<Resident, Integer> {}
+public interface ResidentRepository extends JpaRepository<Resident, Integer> {
+	boolean existsByUsername (String username);
+}
 
